5a2eb097e48d91eac793f5d1af4e1678f770fc4c
Brent Bolleman <bbolleman@gmail.com>
2024-10-10 11:36:18 -0700

    remove from index

--- app/functions.js
+++
@@ -1,6 +0,0 @@
-import { py, pyarr } from "./tasks/pyrun/controller.js";
-import { runPy } from "./runpy/controller.js";
-
-CustomFunctions.associate("PY", py);
-CustomFunctions.associate("PY.ARR", pyarr);
-CustomFunctions.associate("PY.BETA", runPy);
