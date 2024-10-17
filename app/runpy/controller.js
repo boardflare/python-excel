e1513517378118f5fc4394d53ae8aaee12183ff3
Brent Bolleman <bbolleman@gmail.com>
2024-10-17 10:04:37 -0700

    fix indexddb bug, error handling

--- app/runpy/controller.js
+++ app/runpy/controller.js
@@ -1,19 +1,17 @@
 import { queueTask } from '../utils/common.js';
 import { fetchCode } from '../utils/fetchcode.js';
 
-// Initialize the Pyodide worker
 let pyworker = new Worker(new URL('./pyodide-worker.js', import.meta.url));
 
-// Function to send messages to the worker and handle responses
 async function messageWorker(worker, message) {
     return new Promise((resolve, reject) => {
         worker.onmessage = (event) => {
             const { result, stdout, error } = event.data;
             if (error) {
-                console.error('Worker error:', error);
+                console.error('Worker successfully returned message but with an error:', error, 'stdout:', stdout);
                 reject({ error, stdout });
             } else {
-                console.log('Worker result:', result);
+                console.log('Worker successfully returned result:', result, 'stdout:', stdout);
                 resolve({ result, stdout });
             }
         };
@@ -25,18 +23,14 @@ async function messageWorker(worker, message) {
     });
 }
 
-// Function to run Python code using the Pyodide worker
 async function runPython({ code, data1 }) {
+    const progressDiv = document.getElementById('progress');
     try {
-        // Fetch code from URL, or use as plain string
         code = await fetchCode(code);
-
         const { result, stdout } = await messageWorker(pyworker, { code, data1 });
 
-        // Write stdout to the progress div
-        document.getElementById('progress').innerText = stdout;
+        progressDiv.innerText += `STDOUT: \n${stdout}`;
 
-        // Conditionally emit gtag event
         if (isChromiumOrEdge) {
             window.gtag('event', 'py', { code_length: code.length });
         }
@@ -47,18 +41,15 @@ async function runPython({ code, data1 }) {
         const errorMessage = error.error || error.message;
         const stdout = error.stdout || '';
         console.error('Error in runPython:', errorMessage);
-        document.getElementById('progress').innerText = `${stdout}\n${errorMessage}`;
+        progressDiv.innerText += `\n${stdout}\n${errorMessage}`;
 
-        // Conditionally emit gtag error event
         if (isChromiumOrEdge) {
             window.gtag('event', 'py_err', { error: errorMessage });
         }
-        const notice = "Error, see console for details.";
-        return [[notice]];
+        return [[`Error, see console for details.`]];
     }
 }
 
-// Export the function to be used in `functions.js`
 export async function runPy(code, data1) {
     const args = { code, data1 };
     return await queueTask(args, runPython);
--- app/utils/fetchcode.js
+++ app/utils/fetchcode.js
@@ -1,83 +1,126 @@
-// Helper function to fetch code from IndexedDB
 async function fetchJupyterlite({ path }) {
-    return new Promise((resolve, reject) => {
-        // Ensure the IndexedDB name is unique to avoid conflicts
+    try {
+        const databases = await indexedDB.databases();
         const dbName = 'JupyterLite Storage';
-        const request = indexedDB.open(dbName);
 
-        request.onsuccess = (event) => {
-            console.log('IndexedDB opened successfully.');
-            const db = event.target.result;
+        console.log('Databases in IndexedDB:', databases);
+        const dbExists = databases.some(db => db.name === dbName);
+        if (!dbExists) {
+            throw new Error(`Database "${dbName}" does not exist in IndexedDB.`);
+        }
 
-            // Log the current version of the database
-            console.log('Current IndexedDB version:', db.version);
+        return new Promise((resolve, reject) => {
+            const request = indexedDB.open(dbName);
 
-            const transaction = db.transaction(['files'], 'readonly');
-            const store = transaction.objectStore('files');
+            request.onsuccess = (event) => {
+                console.log('IndexedDB opened successfully.');
+                const db = event.target.result;
+                console.log('Current IndexedDB version:', db.version);
 
-            const getRequest = store.get(path);
-            getRequest.onsuccess = (event) => {
-                if (event.target.result) {
-                    console.log('Result from key in IndexedDB:', event.target.result.content);
-                    resolve(event.target.result.content);
-                } else {
-                    console.error('No code found for the given key in IndexedDB.');
-                    reject(new Error(`File not found in Jupyter: ${path}`));
+                if (!db.objectStoreNames.contains('files')) {
+                    console.error('Object store "files" does not exist in IndexedDB.');
+                    reject(new Error('Object store "files" does not exist in IndexedDB.'));
+                    return;
                 }
+
+                const transaction = db.transaction(['files'], 'readonly');
+                const store = transaction.objectStore('files');
+
+                const getRequest = store.get(path);
+                getRequest.onsuccess = (event) => {
+                    if (event.target.result) {
+                        console.log('Result from key in IndexedDB:', event.target.result.content);
+                        let content = event.target.result.content;
+                        if (path.endsWith('.ipynb')) {
+                            const cells = content.cells.filter(cell => cell.cell_type === 'code');
+                            const pyoutCell = cells.find(cell => cell.source.includes('pyout'));
+                            if (pyoutCell) {
+                                const pyoutCellSource = pyoutCell.source;
+                                console.log('Code cell containing "pyout":', pyoutCellSource);
+                                resolve(pyoutCellSource);
+                                return;
+                            } else {
+                                reject(new Error('No code cell containing "pyout" found.'));
+                                return;
+                            }
+                        }
+                        resolve(content);
+                    } else {
+                        console.error('No code found for the given key in IndexedDB.');
+                        reject(new Error(`File not found in Jupyter: ${path}`));
+                    }
+                };
+
+                getRequest.onerror = (event) => {
+                    console.error('Error fetching code from IndexedDB:', event.target.error);
+                    reject(event.target.error);
+                };
             };
 
-            getRequest.onerror = (event) => {
-                console.error('Error fetching code from IndexedDB:', event.target.error);
+            request.onerror = (event) => {
+                console.error('Error opening IndexedDB:', event.target.error);
                 reject(event.target.error);
             };
-        };
-
-        request.onerror = (event) => {
-            console.error('Error opening IndexedDB:', event.target.error);
-            reject(event.target.error);
-        };
-    });
+        });
+    } catch (error) {
+        console.error('Error listing databases:', error);
+        throw error;
+    }
 }
 
-// Helper function to fetch code from a URL
 export async function fetchCode(source) {
     let code;
-
     if (source.startsWith('https://')) {
-        const response = await fetch(source);
-        if (!response.ok) {
-            throw new Error(`Failed to fetch code from URL: ${response.statusText}`);
+        try {
+            const response = await fetch(source);
+            if (!response.ok) {
+                if (response.status === 404) {
+                    throw new Error(`URL does not exist:\n ${source}\n Status: ${response.status} ${response.statusText}`);
+                } else if (response.status === 401) {
+                    throw new Error(`URL requires authorization:\n ${source}\n Status: ${response.status} ${response.statusText}`);
+                } else {
+                    throw new Error(`Error fetching code from:\n ${source}\n Status: ${response.status}`);
+                }
+            }
+            code = await response.text();
+            console.log('Code fetched from URL:', code);
+        } catch (error) {
+            if (error instanceof TypeError) {
+                throw new Error(`Error fetching code from URL:\n ${source}\n This might be due to missing CORS headers.\n Original error: ${error.message}`);
+            } else {
+                throw new Error(`Error fetching code from URL:\n ${source}\n Error: ${error.message}`);
+            }
         }
-        code = await response.text();
-        console.log('Code fetched from URL:', code);
     } else if (source.endsWith('.ipynb') || source.endsWith('.py')) {
         try {
-            code = await fetchJupyterlite({ path: source });
-        } catch (error) {
-            console.warn('Failed to fetch from IndexedDB, trying local path:', error);
             const response = await fetch(`./notebooks/${source}`);
             if (!response.ok) {
                 throw new Error(`Failed to fetch code from local path: ${response.statusText}`);
             }
             code = await response.text();
+            if (source.endsWith('.ipynb')) {
+                code = JSON.parse(code);
+                const cells = code.cells.filter(cell => cell.cell_type === 'code');
+                const pyoutCell = cells.find(cell => cell.source.join('').includes('pyout'));
+                if (pyoutCell) {
+                    const pyoutCellSource = pyoutCell.source.join('');
+                    console.log('Code cell containing "pyout":', pyoutCellSource);
+                    return pyoutCellSource;
+                } else {
+                    throw new Error('No code cell containing "pyout" found.');
+                }
+            }
             console.log('Code fetched from local path:', code);
-        }
-
-        if (source.endsWith('.ipynb')) {
-            code = JSON.parse(code);
-            const cells = code.cells.filter(cell => cell.cell_type === 'code');
-            const pyoutCell = cells.find(cell => cell.source.join('').includes('pyout'));
-            if (pyoutCell) {
-                const pyoutCellSource = pyoutCell.source.join('');
-                console.log('Code cell containing "pyout":', pyoutCellSource);
-                return pyoutCellSource;
-            } else {
-                throw new Error('No code cell containing "pyout" found.');
+        } catch (error) {
+            try {
+                code = await fetchJupyterlite({ path: source });
+            } catch (fetchJupyterliteError) {
+                throw new Error(`Error fetching code from JupyterLite: ${fetchJupyterliteError.message}`);
             }
         }
     } else {
         console.log('Using plain code string:', source);
-        code = source; // Assume it's a plain code string
+        code = source;
     }
 
     return code;
