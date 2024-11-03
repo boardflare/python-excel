165ae27e632966ff51ce38d8ef8307c3670e82b6
Brent Bolleman <bbolleman@gmail.com>
2024-11-02 17:39:18 -0700

    remove jupyterlite, function association for py, py.arr

--- app/functions.js
+++ app/functions.js
@@ -1,6 +1,3 @@
-import { py, pyarr } from "./tasks/pyrun/controller.js";
 import { runPy } from "./runpy/controller.js";
 
-CustomFunctions.associate("PY", py);
-CustomFunctions.associate("PY.ARR", pyarr);
 CustomFunctions.associate("RUNPY", runPy);
--- app/utils/fetchcode.js
+++ app/utils/fetchcode.js
@@ -1,73 +1,3 @@
-async function fetchJupyterlite({ path }) {
-    try {
-        const databases = await indexedDB.databases();
-        const dbName = 'JupyterLite Storage';
-
-        console.log('Databases in IndexedDB:', databases);
-        const dbExists = databases.some(db => db.name === dbName);
-        if (!dbExists) {
-            throw new Error(`Database "${dbName}" does not exist in IndexedDB.`);
-        }
-
-        return new Promise((resolve, reject) => {
-            const request = indexedDB.open(dbName);
-
-            request.onsuccess = (event) => {
-
-                const db = event.target.result;
-
-                if (!db.objectStoreNames.contains('files')) {
-                    console.error('Object store "files" does not exist in IndexedDB.');
-                    reject(new Error('Object store "files" does not exist in IndexedDB.'));
-                    return;
-                }
-
-                const transaction = db.transaction(['files'], 'readonly');
-                const store = transaction.objectStore('files');
-
-                const getRequest = store.get(path);
-                getRequest.onsuccess = (event) => {
-                    if (event.target.result) {
-                        console.log('Result from key in IndexedDB:', event.target.result.content);
-                        let content = event.target.result.content;
-                        if (path.endsWith('.ipynb')) {
-                            const cells = content.cells.filter(cell => cell.cell_type === 'code');
-                            const functionCell = cells.find(cell => cell?.metadata?.tags?.includes('function'));
-                            if (functionCell) {
-                                // Jupyterlite stores source as a single string, not an array of strings
-                                const functionCellSource = functionCell.source;
-                                console.log('Code cell containing "function" tag:', functionCellSource);
-                                resolve(functionCellSource);
-                                return;
-                            } else {
-                                reject(new Error('No code cell containing "function" tag found.'));
-                                return;
-                            }
-                        }
-                        resolve(content);
-                    } else {
-                        console.error('No code found for the given key in IndexedDB.');
-                        reject(new Error(`File not found in Jupyter: ${path}`));
-                    }
-                };
-
-                getRequest.onerror = (event) => {
-                    console.error('Error fetching code from IndexedDB:', event.target.error);
-                    reject(event.target.error);
-                };
-            };
-
-            request.onerror = (event) => {
-                console.error('Error opening IndexedDB:', event.target.error);
-                reject(event.target.error);
-            };
-        });
-    } catch (error) {
-        console.error('Error listing databases:', error);
-        throw error;
-    }
-}
-
 export async function fetchCode(source) {
     let code;
     if (source.startsWith('https://')) {
@@ -102,7 +32,7 @@ export async function fetchCode(source) {
                 throw new Error(`Error fetching code from URL:\n ${source}\n Error: ${error.message}`);
             }
         }
-        // Loads code using path only from prod or jupyterlite
+        // Loads code using path only
     } else if (source.endsWith('.ipynb') || source.endsWith('.py')) {
         try {
             const response = await fetch(`https://functions.boardflare.com/notebooks/${source}`);
@@ -123,11 +53,7 @@ export async function fetchCode(source) {
                 }
             }
         } catch (error) {
-            try {
-                code = await fetchJupyterlite({ path: source });
-            } catch (fetchJupyterliteError) {
-                throw fetchJupyterliteError;
-            }
+            throw error; // Simply rethrow the error instead of trying jupyterlite
         }
     } else {
         // Use code string as is
